f8a19966b57d49aec9d0a67507e483a9d4130aaa
sanjana3029 <114164939+sanjana3029@users.noreply.github.com>
2024-03-04 09:34:25 +0530

    second commit

---
+++ Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Customer
+    {
+        private static int CustomerID;
+        private static string Name;
+        private static int Age;
+        private static int PhoneNo;
+        private static string City;
+
+        public Customer()
+        {
+
+        }
+        public Customer(int CustId, string name, int age, int Phno, string city)
+        {
+            CustomerID = CustId;
+            Name = name;
+            Age = age;
+            PhoneNo = Phno;
+            City = city;
+        }
+
+        static void DisplayCustomer()
+        {
+            Console.WriteLine($"Customer Id: {CustomerID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Phone Number: {PhoneNo}");
+            Console.WriteLine($"City: {City}");
+        }
+        ~Customer()       // ~ for destructor
+        {
+            Console.WriteLine("object destroy");
+            Console.Read();
+        }
+        public static void Main()
+            {
+                Customer customer1 = new Customer(9, "Sanju", 21, 8378326, "Lucknow");
+                Customer.DisplayCustomer();
+                Console.Read();
+            }
+
+        }
+     }
+
+
--- Program.cs
+++ Program.cs
@@ -4,32 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test_code
+namespace Assignment_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(test("Python",1));
-            //Console.WriteLine(test("Python", 0));
-            //Console.WriteLine(test("Python", 4));
+            //Console.Write("roll no: ");
+            //int rollno = int.Parse(Console.ReadLine());
+
+            //Console.Write("name: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("sem: ");
+            //string sem = Console.ReadLine();
+
+            //Console.Write("stclass: ");
+            //string stclass = Console.ReadLine();
+
+            //Console.Write("branch: ");
+            //string branch = Console.ReadLine();
 
 
 
-            //Largest_No lr = new Largest_No();
-            //lr.Check();
+            //Student st = new Student(rollno, name, sem, stclass, branch);
+            ////Student st = new Student(23, "shreya", "sem1", "12A","cse");
+            //st.GetMarks();
+            //st.DisplayData();
+            //st.DisplayResult();
+
+
+            //Console.Write("salesno : ");
+            //int salesno = int.Parse(Console.ReadLine());
+
+            //Console.Write("Productno: ");
+            //int Productno = int.Parse(Console.ReadLine());
+
+            //Console.Write("Price: ");
+            //double Price = double.Parse(Console.ReadLine());
+
+            //Console.Write("Qty: ");
+            //int Qty = int.Parse(Console.ReadLine());
+
+            //Console.Write("Date of Sale (d/M/yyyy): ");
+            //string dateofsale = Console.ReadLine();
 
 
 
-            Exchange ex = new Exchange();
-            ex.Characters();
+            //SaleDetails sd = new SaleDetails( salesno, Productno, Price,  dateofsale, Qty);
+            //SaleDetails sd = new SaleDetails(101, 55, 128, "7/7/2022", 7);
+            //sd.Sales();
+            //sd.Showdata();
+            //Console.Read();
 
-            Console.ReadLine();
-        }
-        public static string test(string str, int n)
-        {
-            return str.Remove(n, 1);
-        }
 
+
+            //Bank bk = new Bank(1234565,"Sanjana","Saving",'d',10000,15000);
+
+            //bk.Update();
+            //Console.WriteLine("------------Information After Transaction------------");
+            //bk.ShowData();
+
+           
+
+    
+
+
+    }
     }
 }
